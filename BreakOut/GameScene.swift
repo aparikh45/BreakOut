a6271a78f6e47666d5e493134136b88942138b6b
Arnav Parikh <arnavparikh1@gmail.com>
2019-05-30 09:03:11 -0500

    finished

--- BreakOut/GameScene.swift
+++ BreakOut/GameScene.swift
@@ -107,6 +107,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 }
             }
         }
+        
         if contact.bodyA.node?.name == "loseZone" ||
             contact.bodyB.node?.name == "loseZone" {
             lives -= 1
